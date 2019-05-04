74b5b4ada4f3e4e2ea6c87548d2554c632a7ddc4
rachakrit <vxlo_olxv@hotmail.com>
2019-05-04 23:44:57 +0700

    เพิ่ม upload

--- server/src/middleware/index.js
+++ server/src/middleware/index.js
@@ -1,5 +1,30 @@
+const uploadRes = require('./upload-res');
+const multer = require('multer');
+const path = require('path');
+const crypto = require("crypto");
+
+
+
+const allowedExtentions = ['.png', '.jpeg']
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => cb(null, 'public/uploads'),
+  filename: (req, file, cb) => {
+    let id = crypto.randomBytes(2).toString('hex');
+    let ext = path.extname(file.originalname);
+    cb(null, `${Date.now()}-${id}${ext}`)
+  }
+});
+
+const upload = multer({
+  storage,
+  limits: {
+    fieldSize: 100 // 100 MB
+  },
+  fileFilter: (req, file, cb) => cb(null, allowedExtentions.includes(path.extname(file.originalname)))
+});
+
+
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
-  // Add your custom middleware here. Remember that
-  // in Express, the order matters.
+  app.post('/upload', upload.array('files'), uploadRes());
 };
