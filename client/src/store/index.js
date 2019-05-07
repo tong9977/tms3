7b309d74920fda6a1061734e115e81ab121700f3
suriyapong <suriyapong_s@hotmail.com>
2019-05-07 12:19:45 +0700

    update store

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -33,13 +33,10 @@ const store = new Vuex.Store({
         service('jobtype',{modelName:'JobType' ,idField:'Id'}),
         service('jobstatus',{modelName:'JobStatus' ,idField:'Id'}),
         service('jobitem',{idField:'Id'}),
-<<<<<<< HEAD
         service('trips',{idField:'TripId'}),
         service('tripservice',{idField:'Id'}),
-=======
         service('product',{idField:'Id'}),
         service('unit',{idField:'Id'})
->>>>>>> 19c6d94ef0af25d5ff1f86658c0372aeb5fbdc57
     ]
 })
 
