f7f7d054f298780edd2e428660e345e2d2c5403b
punnz <pzmozi@gmail.com>
2019-05-08 23:20:33 +0700

    UploadJobDetail

--- server/src/services/index.js
+++ server/src/services/index.js
@@ -10,15 +10,12 @@ const trips = require('./trips/trips.service.js');
 const unit = require('./unit/unit.service.js');
 const product = require('./product/product.service.js');
 const usertrip = require('./usertrip/usertrip.service.js');
-<<<<<<< HEAD
 const usertripservice = require('./usertripservice/usertripservice.service.js');
 const upload = require('./upload/upload.service.js');
-=======
 const tripcommand = require('./tripcommand/tripcommand.service.js');
 const usertripcommand = require('./usertripcommand/usertripcommand.service.js');
 const jobtrip = require('./jobtrip/jobtrip.service.js');
 const jobtripcommand = require('./jobtripcommand/jobtripcommand.service.js');
->>>>>>> 23beab1237c9032f3d983f2914715cb12cb02dd3
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(role);
@@ -33,13 +30,10 @@ module.exports = function (app) {
   app.configure(unit);
   app.configure(product);
   app.configure(usertrip);
-<<<<<<< HEAD
   app.configure(usertripservice);
   app.configure(upload);
-=======
   app.configure(tripcommand);
   app.configure(usertripcommand);
   app.configure(jobtrip);
   app.configure(jobtripcommand);
->>>>>>> 23beab1237c9032f3d983f2914715cb12cb02dd3
 };
