3eb5c906914abee5352645f7aa2ca2cd7ef48d2b
punnz <pzmozi@gmail.com>
2019-05-14 21:02:48 +0700

    AddMytripAndTripDetail

--- client/src/views/MyTripMobile.vue
+++ client/src/views/MyTripMobile.vue
@@ -1,32 +1,39 @@
 <template>
   <v-container>
-    <v-btn color="blue" @click="$router.go(-1)">ย้อนกลับ</v-btn>
-
     <material-card color="green" title="รายละเอียดการงานประจำสัปดาห์">
-      
-    
-       
-          <v-list two-line >
-          <template v-for="item in usertrips" >
-              
-            <v-list-tile :key="item.Id" @click="GotoJobList(item.Id)">
-              <v-list-tile-action>
-                <v-icon color="blue">mdi-file-document</v-icon>
-              </v-list-tile-action>
-              <v-list-tile-content >
-                <v-list-tile-title>  รหัสงาน : {{item.Id}} Approve <v-icon v-if="item.Approve">mdi-check</v-icon>  <v-icon v-else>mdi-close</v-icon></v-list-tile-title>
-                <v-list-tile-sub-title > ทะเบียนรถ : {{item.vehicle.LicensePlate}}</v-list-tile-sub-title>
-                <v-list-tile-sub-title >วันที่ต้องส่งสินค้า : {{item.jobs[0].RequestedDate | date}} </v-list-tile-sub-title>
-              </v-list-tile-content>
-              <v-list-tile-action>
-               
-              </v-list-tile-action>
-            </v-list-tile>  
-            <v-divider :key="item.Id"></v-divider>
-            </template>
-          </v-list> 
-        
-       </material-card>
+      <v-layout>
+        <v-flex xs4 md4 text-xs-center>
+          <v-btn @click="Backweek()" flat small color="primary">
+            <v-icon>mdi-arrow-left</v-icon>
+          </v-btn>
+        </v-flex>
+        <v-flex xs4 md4 text-xs-center>
+          <h4>{{start|date2}} - {{end|date2}}</h4>
+        </v-flex>
+        <v-flex xs4 md4 text-xs-center>
+          <v-btn @click="Nextweek()" flat small color="primary">
+            <v-icon>mdi-arrow-right</v-icon>
+          </v-btn>
+        </v-flex>
+      </v-layout>
+
+      <v-list two-line>
+        <template v-for="item in usertrips">
+          <v-list-tile :key="item.Id" @click="GotoJobList(item.Id)">
+            <v-list-tile-action>
+              <v-icon color="blue">mdi-car-pickup</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>วันส่งสินค้า : {{item.TripDate | date}}</v-list-tile-title>
+              <v-list-tile-sub-title>ทะเบียนรถ : {{item.vehicle.LicensePlate}}</v-list-tile-sub-title>
+              <v-list-tile-sub-title>รหัสงาน : {{item.Id}} </v-list-tile-sub-title>
+            </v-list-tile-content>
+            <v-list-tile-action></v-list-tile-action>
+          </v-list-tile>
+          <v-divider :key="item.Id"></v-divider>
+        </template>
+      </v-list>
+    </material-card>
   </v-container>
 </template>
 
@@ -41,7 +48,10 @@ import {
   endOfDay,
   endOfMonth,
   lastDayOfYear,
-  format
+  format,
+  startOfWeek,
+  endOfWeek,
+  addDays
 } from "date-fns";
 import { createDateFilter } from "vue-date-fns";
 import locale from "date-fns/locale/th";
@@ -53,40 +63,51 @@ export default {
     service: "usertripcommand",
     tripDate: new Date(),
     usertrips: [],
+    total: 0,
+    start: "",
+    end: ""
   }),
-
 
   computed: {},
   filters: {
-    date: createDateFilter("DD/MM/YYYY", { locale })
+    date: createDateFilter("ddd" + " DD MMM YYYY", { locale }),
+    date2: createDateFilter("DD/MMM/YYYY", { locale })
   },
-  async mounted()  {
+  async mounted() {
     this.render();
   },
   methods: {
     async render() {
       try {
-        var x = startOfDay(new Date(this.tripDate));
-        var y = startOfDay(new Date(this.tripDate));
-        let start = format(x, "2019-05-13T00:00:00");
-        let end = format(y, "2019-05-16T23:59:59");
+        var x = startOfWeek(new Date(this.tripDate), { weekStartsOn: 1 });
+        var y = endOfWeek(new Date(this.tripDate), { weekStartsOn: 1 });
+        this.start = format(x, "YYYY-MM-DD");
+        this.end = format(y, "YYYY-MM-DD");
 
         let res = await this.$store.dispatch("usertripcommand/find", {
-          query: {UserId:8 ,Start: start,End: end }
+          query: { UserId: 8, Start: this.start, End: this.end }
         });
+
+       // this.total = res[0].Trips[0].jobs.length;
+
         this.usertrips = res[0].Trips;
-        //alert(JSON.stringify(this.usertrip))
       } catch (error) {
         console.log(error);
         alert("ไม่สามารถติดต่อ server ได้");
       }
-
     },
     GotoJobList(Id) {
-     // this.$router.push({ path: `/TripDetailMobile/${Id}` })
-       this.$router.push({ name: "TripDetailMobile", params: { Id: Id } });
+      // this.$router.push({ path: `/TripDetailMobile/${Id}` })
+      this.$router.push({ name: "TripDetailMobile", params: { Id: Id } });
+    },
+    Nextweek() {
+      this.tripDate = addDays(this.tripDate, 7);
+      this.render();
+    },
+    Backweek() {
+      this.tripDate = addDays(this.tripDate, -7);
+      this.render();
     }
-  },
-  
+  }
 };
 </script>
--- client/src/views/TripDetailMobile.vue
+++ client/src/views/TripDetailMobile.vue
@@ -1,32 +1,30 @@
 <template>
   <v-container>
     <v-btn color="blue" @click="$router.go(-1)">ย้อนกลับ</v-btn>
-
+   
     <material-card color="green" title="รายละเอียดการงานประจำสัปดาห์ ">
-      
-    {{usertrips}}
-       
-          <v-list two-line >
-          <template v-for="item in usertrips" >
-              
-            <v-list-tile :key="item.Id" @click="GotoJobList(item.jobs[0].Id)">
-              <v-list-tile-action>
-                <v-icon color="blue">mdi-file-document</v-icon>
-              </v-list-tile-action>
-              <v-list-tile-content >
-                <v-list-tile-title>  ลูกค้า : {{item.jobs[0].Customer}} ที่อยู่ : {{item.jobs[0].Address}}</v-list-tile-title>
-                <v-list-tile-sub-title > ทะเบียนรถ : {{item.vehicle.LicensePlate}}</v-list-tile-sub-title>
-                <v-list-tile-sub-title >วันที่ต้องส่งสินค้า : {{item.jobs[0].RequestedDate | date}} </v-list-tile-sub-title>
-              </v-list-tile-content>
-              <v-list-tile-action>
-             
-              </v-list-tile-action>
-            </v-list-tile>  
-            <v-divider :key="item.Id"></v-divider>
-            </template>
-          </v-list> 
+      <v-list three-line>
+        <template v-for="item in tripsdetail">
+          <template v-for="i in item.jobs">
+          <v-list-tile @click="GotoJobList(i.Id)" :key="i">
         
-       </material-card>
+          
+                <v-list-tile-action>
+                  <v-icon color="blue">mdi-file-document</v-icon>
+                </v-list-tile-action>
+                <v-list-tile-content>
+                  <v-list-tile-title>วันที่ต้องส่งสินค้า : {{i.RequestedDate | date}}</v-list-tile-title>
+                  <v-list-tile-sub-title>รหัสงาน : {{i.Id}} </v-list-tile-sub-title>
+                  <v-list-tile-sub-title>ลูกค้า : {{i.Customer}} ที่อยู่ : {{i.Address}}</v-list-tile-sub-title>
+                </v-list-tile-content>
+             
+          </v-list-tile>
+          <v-divider :key="i"></v-divider>
+          </template>
+          
+        </template>
+      </v-list>
+    </material-card>
   </v-container>
 </template>
 
@@ -50,43 +48,37 @@ export default {
   components: {},
 
   data: () => ({
-    service: "usertripcommand",
     tripDate: new Date(),
-    usertrips: [],
+    tripsdetail: []
   }),
-
 
   computed: {},
   filters: {
-    date: createDateFilter("DD/MM/YYYY", { locale })
+    date: createDateFilter("ddd" + " DD MMM YYYY", { locale }),
   },
-  async mounted()  {
+  async mounted() {
     this.render();
   },
   methods: {
     async render() {
+      
       try {
-        var x = startOfDay(new Date(this.tripDate));
-        var y = startOfDay(new Date(this.tripDate));
-        let start = format(x, "2019-05-13T00:00:00");
-        let end = format(y, "2019-05-16T23:59:59");
-        let IdTrip =177;
-        //alert(JSON.stringify(IdTrip))
+        let IdTrip = this.$route.params.Id;
+        
 
-        let res = await this.$store.dispatch("usertripcommand/find", {
-          query: {UserId:8 ,Start: start,End: end} } );
-        this.usertrips = res[0].Trips;
-        //alert(JSON.stringify(this.usertrip))
+        let res = await this.$store.dispatch("trips/find", {
+          query: { Id: IdTrip, $eager: "[vehicle,users,jobs]" }
+        });
+        this.tripsdetail = res.data;
       } catch (error) {
         console.log(error);
-        alert("ไม่สามารถติดต่อ server ได้");
+        this.$toast.error("ไม่สามารถติดต่อ server ได้");
       }
-
     },
     GotoJobList(Id) {
-      this.$router.push({ path: `/jobdetail/${Id}` })
+      this.$router.push({ path: `/jobdetailmobile/${Id}` });
+
     }
-  },
-  
+  }
 };
 </script>
