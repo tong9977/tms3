50d89b2eaa17b29aa95d0aa1e5432a9b31ce69a3
rachakrit <vxlo_olxv@hotmail.com>
2019-05-16 00:01:03 +0700

    เพิ่ม user Id

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -2,11 +2,12 @@
 export default {
   mounted() {
     const roleId = this.$store.state.auth.user.RoleId;
+    const userId = this.$store.state.auth.user.Id;
     if (roleId === 1) {
       this.$router.push('/dashboard');
     }
     if (roleId === 2) {
-      this.$router.push('/todaytrip');
+      this.$router.push('/mytripmobile/'+userId);
     }
   }
 };
