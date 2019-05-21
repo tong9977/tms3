ac50fa2c08dccc23def206f97b2115fb51d43d00
suriyapong <suriyapong_s@hotmail.com>
2019-05-21 20:00:14 +0700

    เพิ่ม Rate

---
+++ client/src/viewComponents/RateComp.vue
@@ -0,0 +1,100 @@
+<template>
+  <v-container fill-height fluid grid-list-xl>
+  <v-layout>
+    <div full-width v-if="mode == 'rate'">
+      <v-layout row wrap>
+        <div>ประเมินงาน [{{ JobId }}]</div>
+        <v-card-text>
+          <v-flex xs12 md12 lg12 v-for="q in rateQuestions" :key="q.Id">
+            <p>ข้อที่ {{ q.Id }} {{ q.Question }}</p>
+            <v-radio-group v-model="q.Id" row>
+              <v-radio label="แย่มาก" value="1"></v-radio>
+              <v-radio label="แย่" value="2"></v-radio>
+              <v-radio label="ปานกลาง" value="3"></v-radio>
+              <v-radio label="ดี" value="4"></v-radio>
+              <v-radio label="ดีมาก" value="5"></v-radio>
+            </v-radio-group>
+          </v-flex>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" flat :loading="loading" @click="saveRateToServer()">ยืนยัน</v-btn>
+        </v-card-actions>
+      </v-layout>
+    </div>
+
+    <div full-width v-if="mode == 'view'">
+      <p>ประเมินงาน [{{ JobId }}]</p>
+      <v-layout row wrap>
+        <v-card-text>
+          <v-flex xs12 md12 lg12 v-for="q in rateQuestions" :key="q.Id">
+            <p>ข้อที่ {{ q.Id }} {{ q.Question }}</p>
+            <div>
+              <p v-if="rateResult[q.Id -1] == 1">- แย่มาก</p>
+              <p v-if="rateResult[q.Id -1] == 2">- แย่</p>
+              <p v-if="rateResult[q.Id -1] == 3">- ปานกลาง</p>
+              <p v-if="rateResult[q.Id -1] == 4">- ดี</p>
+              <p v-if="rateResult[q.Id -1] == 5">- ดีมาก</p>
+            </div>
+          </v-flex>
+        </v-card-text>
+      </v-layout>
+    </div>
+  </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    service: "ratequestion",
+    mode: "rate",
+    rateQuestions: [],
+    rateResult: [],
+    rateTotal: []
+  }),
+  props: ["JobId"],
+  mounted: function() {
+    this.render();
+  },
+  methods: {
+    async render() {
+      //find ใหม่ เอาแค่ตัวเดียวที่เพิ่มแล้วสั่ง this.Trip = ที่ get มาใหม่
+      let res = await this.$store.dispatch(this.service + "/find", {});
+      this.rateQuestions = res.data;
+
+      let res2 = await this.$store.dispatch("job/find", {
+        query: { Id: this.JobId }
+      });
+      if (res2.data[0].Rate != null) {
+        //เคยประเมินแล้ว
+        this.rateResult = JSON.parse("[" + res2.data[0].Rate + "]");
+        //เช็คว่าผลการโหวดกับจำนวนข้อเท่ากันไหม
+        if (this.rateResult.length == this.rateQuestions.length) {
+          //เท่ากัน render
+          this.mode = "view";
+        } else {
+          //ไม่เท่ากัน ทำการเซต Rate in Jobs = null
+          this.mode = "rate";
+        }
+      }
+    },
+    async saveRateToServer() {
+      try {
+        this.rateQuestions.forEach(element => {
+          this.rateTotal.push(element.Id);
+        });
+
+        await this.$store.dispatch("job/patch", [
+          this.JobId,
+          { Rate: this.rateTotal }
+        ]);
+        this.render();
+      } catch (err) {
+        console.log(err);
+        alert("ไม่สามารถประเมินได้");
+      }
+    }
+  }
+};
+</script>
