bcbdb982394671d2a76f40b40fbc2a6912e7fb51
suriyapong <suriyapong_s@hotmail.com>
2019-05-22 19:43:35 +0700

    add package email and test sent email

--- server/src/hooks/email.js
+++ server/src/hooks/email.js
@@ -4,6 +4,35 @@
 // eslint-disable-next-line no-unused-vars
 module.exports = function (options = {}) {
   return async context => {
+    const job = context.result;
+    console.log("Email : " + job.Email);
+
+    const nodemailer = require('nodemailer');
+
+    // config สำหรับของ gmail
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: 'icesoftware2016m@gmail.com', // your email
+        pass: 'Sinetong9977' // your email password
+      }
+    });
+
+    let mailOptions = {
+      from: 'icesoftware2016m@gmail.com',                // sender
+      to: 'suriyapong_s@hotmail.com',                // list of receivers
+      subject: 'Hello from sender',              // Mail subject
+      html: '<b>Do you receive this mail?</b>'   // HTML body
+    };
+
+    transporter.sendMail(mailOptions, function (err, info) {
+      if (err)
+        console.log(err)
+      else
+        console.log(info);
+    });
+
+
     return context;
   };
 };
